8717b90eefc5e2bc4aac45393aea1ef0b334e238
Maggie Love <maggie.love1991@gmail.com>
2015-11-07 20:31:11 -0500

    default user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :posts
   has_many :cities
-  has_attached_file :image, styles: { medium: "500x400>", thumb: "100x100>" }
+  has_attached_file :image, :default_url => "missing.png", styles: { medium: "500x400>", thumb: "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+  # def set_default_image
+  #   image_tag "/assets/images/anonymous_user_gravatar.png"
+  # end
 end
